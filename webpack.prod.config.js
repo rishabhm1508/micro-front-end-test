83111180a2727c4b3e9cddd8712b7e6192bf0a28
Rishabh Mishra <rishabhm1508@gmail.com>
2021-07-01 16:58:55 +0530

    webpack-configurations

---
+++ webpack.prod.config.js
@@ -0,0 +1,28 @@
+const webpack = require("webpack");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  output: {
+    publicPath: "https://angular-micro-front-end-test.herokuapp.com/",
+    uniqueName: "mdmfprofile",
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "profile",
+      library: { type: "var", name: "profile" },
+      filename: "remoteEntry.js",
+      exposes: {
+        AppModule:
+          "src/app/app.module.ts",
+      },
+      shared: {
+        "@angular/core": { eager: true, singleton: true },
+        "@angular/common": { eager: true, singleton: true },
+        "@angular/router": { eager: true, singleton: true },
+      },
+    }),
+  ],
+};
